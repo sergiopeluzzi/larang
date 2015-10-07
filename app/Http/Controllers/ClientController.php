2fb1a7db394ff84466b8801fcacd69867c7ae55b
Sergio Peluzzi <sergiopeluzzi@gmail.com>
2015-10-07 12:33:36 +0000

    Primeira fase do curso

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -42,17 +42,6 @@ class ClientController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
